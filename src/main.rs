fab0e5666dbd6356200109eee5031d3d943e5f4d
rexim <reximkut@gmail.com>
2020-04-19 02:25:09 +0700

    Port to SDL2

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,7 @@
-extern crate glutin_window;
-extern crate graphics;
-extern crate opengl_graphics;
-extern crate piston;
-
-use glutin_window::GlutinWindow as Window;
-use opengl_graphics::{GlGraphics, OpenGL};
-use piston::event_loop::{EventSettings, Events};
-use piston::input::{RenderArgs, RenderEvent, UpdateArgs, UpdateEvent};
-use piston::window::WindowSettings;
-
-pub struct App {
-    gl: GlGraphics,
-    rotation: f64,
-}
+use sdl2::pixels::Color;
+use sdl2::event::Event;
+use sdl2::rect::Rect;
+use std::time::Duration;
 
 const CUBE: [[f64; 3]; 8] = [
     [-1.0,  1.0,  1.0],
@@ -33,70 +22,64 @@ fn to_screen([x0, y0]: [f64; 2], w: f64, h: f64) -> [f64; 2] {
     return [x, y];
 }
 
-impl App {
-    fn render(&mut self, args: &RenderArgs) {
-        use graphics::*;
+const DISTANCE: f64 = 4.0;
+const BACKGROUND: Color = Color::RGB(18, 18, 18);
+const FOREGROUND: Color = Color::RGB(255, 150, 150);
+const SQUARE_SIZE: f64 = 10.0;
 
-        const BACKGROUND: [f32; 4] = [0.18, 0.18, 0.18, 1.0];
-        const FOREGROUND: [f32; 4] = [1.0, 0.5, 0.5, 1.0];
-        const SQUARE_SIZE: f64 = 10.0;
-
-        let square = rectangle::square(0.0, 0.0, SQUARE_SIZE);
-        // let (x, y) = (args.window_size[0] * 0.5,
-        //               args.window_size[1] * 0.5);
-
-        let rotation = self.rotation;
-
-        const DISTANCE: f64 = 4.0;
-
-        self.gl.draw(args.viewport(), |c, gl| {
-            clear(BACKGROUND, gl);
-
-            for [x0, y0, z0] in &CUBE {
-                let x1 = x0 * f64::cos(rotation) + z0 * f64::sin(rotation);
-                let z1 = x0 * f64::sin(rotation) - z0 * f64::cos(rotation);
-
-                let x = x1 / (z1 + DISTANCE);
-                let y = y0 / (z1 + DISTANCE);
-                let [sx, sy] = to_screen(
-                    [x, y],
-                    args.window_size[0],
-                    args.window_size[1]);
-
-                let transform = c
-                    .transform
-                    .trans(sx, sy)
-                    .trans(-SQUARE_SIZE * 0.5, -SQUARE_SIZE * 0.5);
-                rectangle(FOREGROUND, square, transform, gl);
-            }
-        });
-    }
-
-
-    fn update(&mut self, args: &UpdateArgs) {
-        self.rotation += 2.0 * args.dt;
-    }
-}
-
-fn main() {
-    let opengl = OpenGL::V3_2;
-
-    let mut window: Window =
-        WindowSettings::new("spinning-square", [200, 200])
-        .graphics_api(opengl)
-        .exit_on_esc(true)
-        .vsync(true)
+fn main() -> Result<(), String> {
+    let sdl_context = sdl2::init()?;
+    let video_subsystem = sdl_context.video()?;
+    let window = video_subsystem.window("Quaternions", 800, 600)
+        .position_centered()
+        .opengl()
         .build()
-        .unwrap();
+        .map_err(|e| e.to_string())?;
 
-    let mut app = App {
-        gl: GlGraphics::new(opengl),
-        rotation: 0.0,
-    };
+    let mut canvas = window.into_canvas().build().map_err(|e| e.to_string())?;
 
-    let mut events = Events::new(EventSettings::new());
-    while let Some(e) = events.next(&mut window) {
-        e.render_args().map(|ref args| app.render(args));
-        e.update_args().map(|ref args| app.update(args));
+    let mut event_pump = sdl_context.event_pump()?;
+    let mut rotation: f64 = 0.0;
+
+    'running: loop {
+        for event in event_pump.poll_iter() {
+            match event {
+                Event::Quit {..} => {
+                    break 'running
+                },
+                _ => {}
+            }
+
+        }
+
+        canvas.set_draw_color(BACKGROUND);
+        canvas.clear();
+
+        canvas.set_draw_color(FOREGROUND);
+        for [x0, y0, z0] in &CUBE {
+            let x1 = x0 * f64::cos(rotation) + z0 * f64::sin(rotation);
+            let z1 = x0 * f64::sin(rotation) - z0 * f64::cos(rotation);
+
+            let x = x1 / (z1 + DISTANCE);
+            let y = y0 / (z1 + DISTANCE);
+            let (w, h) = canvas.window().size();
+            let [sx, sy] = to_screen([x, y], w as f64, h as f64);
+
+            canvas.fill_rect(Rect::new(
+                (sx - SQUARE_SIZE * 0.5) as i32,
+                (sy - SQUARE_SIZE * 0.5) as i32,
+                SQUARE_SIZE as u32,
+                SQUARE_SIZE as u32))?;
+        }
+
+        canvas.present();
+
+        const FPS: u32 = 30;
+        let dt = 1.0 / FPS as f64;
+        ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / FPS));
+        rotation += 2.0 * dt;
+
     }
+
+    Ok(())
 }
